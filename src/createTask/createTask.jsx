a7426b950d6031077bd9b57f3ed557ce03af5689
Zhifan Lan <everydaysiling@gmail.com>
2016-08-05 21:42:05 +0800

    aaa

--- src/createTask/createTask.jsx
+++ src/createTask/createTask.jsx
@@ -68,69 +68,32 @@ var Slide_button = React.createClass({
     render: function() {
 
         var slideStyle = {
-            backgroundColor: '#3bb3e0',
-            fontSize: '13px',
-            padding: '10px 20px',
-            paddingRight: '50px',
-            borderRadius: '5px',
+            fontSize: '15px',
+            fontFamily:'Lato,Arial,serif',
             float:'left',
-            boxShadow: 'inset 0px 1px 0px #2ab7ec, 0px 5px 0px 0px #156785, 0px 10px 5px #999',
-            backgroundImage: '-webkit-gradient( linear, left bottom, left top, color-stop(0, rgb(44,160,202)), color-stop(1, rgb(62,184,229)) )',
-            '::before': {
-                backgroundColor: '#2591b4',
-                backgroundImage: 'url(../img/hi.png)',
-                backgroundRepeat: 'no-repeat',
-                backgroundPosition: 'center center',
-                content: '',
-                width: '20px',
-                height: '20px',
-                position: 'absolute',
-                right: '15px',
-                top: '50%',
-                marginTop: '-9px',
-                borderRadius: '50%',
-                boxShadow: 'inset 0px 1px 0px #052756, 0px 1px 0px #60c9f0',
-            },
-            ':active': {
-                top: '3px',
-                boxShadow: 'inset 0px 1px 0px #2ab7ec, 0px 2px 0px 0px #156785, 0px 5px 3px #999',
-                backgroundImage: '-webkit-gradient( linear, left bottom, left top, color-stop(0, rgb(62,184,229)), color-stop(1, rgb(44,160,202)) )',
-                backgroundColor: '#3bb3e0',
-                padding: '10px 20px',
-                paddingRight: '50px',
-                borderRadius: '5px',
-                float:'left',
-                fontSize:'13px',
-            },
-            ':active::before':{
-                backgroundColor: '#2591b4',
-                backgroundImage: 'url(../img/hi.png)',
-                backgroundRepeat: 'no-repeat',
-                backgroundPosition: 'center center',
-                content: '',
-                width: '20px',
-                height: '20px',
-                position: 'absolute',
-                right: '15px',
-                top: '50%',
-                marginTop: '-9px',
-                borderRadius: '50%',
-                boxShadow: 'inset 0px 1px 0px #052756, 0px 1px 0px #60c9f0',
-            }
+            fontWeight: 'bold',
+            letterSpacing: '1px',
+            width:'100px'
         };
-        return <a style = {slideStyle} onClick={this.props.data.onClick}> {this.props.data.text} </a>
+        var unClick = {
+            backgroundColor: '#8a98b8',
+            color:'#fbfcfd',
+        };
+        var clicked = {
+            backgroundColor: '#4a5374',
+            color:'#8a98b8',
+        };
+        var clickStyle = this.props.data.clickDown?'clicked':'unClick';
+        return <div className={'slideStyle'+' '+clickStyle} onClick={this.props.data.onClick}> {this.props.data.text} </div>
     }
 });
 var Content = React.createClass({
     render: function() {
 
         var informationStyle = {
-            backgroundColor: '#3bb3e0',
-            '::before': {
-                backgroundColor: '#2591b4',
-            }
+            width:'5px'
         };
-        return <div style = {informationStyle}> this.props.data.text </div>
+        return <div style = {informationStyle}> </div>
     }
 });
 var Container = React.createClass({
@@ -138,10 +101,11 @@ var Container = React.createClass({
         var wholeStyle = {
             boxShadow:'0 0 20px rgba(132,131,131,0.8)',
             width:'500px',
-            height:'600px'
+            height:'600px',
+            margin:'0 auto'
         };
-        return (<div id='container' style = {wholeStyle}><Slide_toggle data={this.props.text}/><Content/></div>)
+        return (<div id='container' style = {wholeStyle}><Slide_toggle text={text}/><Content/></div>)
     }
 });
 
-ReactDOM.render((<Container data={text}/>), document.getElementById('content'));
+ReactDOM.render((<Container/>), document.getElementById('content'));
