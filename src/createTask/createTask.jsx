b495c829d2990939bff83a47a134e3560b196e07
Zhifan Lan <everydaysiling@gmail.com>
2016-08-05 12:35:18 +0800

    newest edition

--- src/createTask/createTask.jsx
+++ src/createTask/createTask.jsx
@@ -61,45 +61,43 @@ var Slide_toggle = React.createClass({
 
 var Slide_button = React.createClass({
     render: function() {
-        var slideStyleClicked = {
-            top: '3px',
-            boxShadow: 'inset 0px 1px 0px #2ab7ec, 0px 2px 0px 0px #156785, 0px 5px 3px #999',
-            backgroundImage: '-webkit-gradient( linear, left bottom, left top, color-stop(0, rgb(62,184,229)), color-stop(1, rgb(44,160,202)) )',
-            backgroundColor: '#3bb3e0',
-            padding: '10px 20px',
-            paddingRight: '50px',
-            borderRadius: '5px',
-            float:'left',
-            fontSize:'13px',
-            ':before': {
-                backgroundColor: '#2591b4',
-                backgroundImage: 'url(../img/hi.png)',
-                backgroundRepeat: 'no-repeat',
-                backgroundPosition: 'center center',
-                content: '',
-                width: '20px',
-                height: '20px',
-                position: 'absolute',
-                right: '15px',
-                top: '50%',
-                marginTop: '-9px',
-                borderRadius: '50%',
-                boxShadow: 'inset 0px 1px 0px #052756, 0px 1px 0px #60c9f0',
-            }
-
-        };
 
         var slideStyle = {
             backgroundColor: '#3bb3e0',
             fontSize: '13px',
-            // width:'40px',
             padding: '10px 20px',
             paddingRight: '50px',
             borderRadius: '5px',
             float:'left',
             boxShadow: 'inset 0px 1px 0px #2ab7ec, 0px 5px 0px 0px #156785, 0px 10px 5px #999',
             backgroundImage: '-webkit-gradient( linear, left bottom, left top, color-stop(0, rgb(44,160,202)), color-stop(1, rgb(62,184,229)) )',
-            ':before': {
+            '::before': {
+                backgroundColor: '#2591b4',
+                backgroundImage: 'url(../img/hi.png)',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center center',
+                content: '',
+                width: '20px',
+                height: '20px',
+                position: 'absolute',
+                right: '15px',
+                top: '50%',
+                marginTop: '-9px',
+                borderRadius: '50%',
+                boxShadow: 'inset 0px 1px 0px #052756, 0px 1px 0px #60c9f0',
+            },
+            ':active': {
+                top: '3px',
+                boxShadow: 'inset 0px 1px 0px #2ab7ec, 0px 2px 0px 0px #156785, 0px 5px 3px #999',
+                backgroundImage: '-webkit-gradient( linear, left bottom, left top, color-stop(0, rgb(62,184,229)), color-stop(1, rgb(44,160,202)) )',
+                backgroundColor: '#3bb3e0',
+                padding: '10px 20px',
+                paddingRight: '50px',
+                borderRadius: '5px',
+                float:'left',
+                fontSize:'13px',
+            },
+            ':active::before':{
                 backgroundColor: '#2591b4',
                 backgroundImage: 'url(../img/hi.png)',
                 backgroundRepeat: 'no-repeat',
@@ -115,10 +113,7 @@ var Slide_button = React.createClass({
                 boxShadow: 'inset 0px 1px 0px #052756, 0px 1px 0px #60c9f0',
             }
         };
-        var showingStyle = slideStyle;
-        if (this.props.data.clickDown)
-            showingStyle = slideStyleClicked;
-        return <div style = {showingStyle} onClick={this.props.data.onClick}> this.props.data.text </div>
+        return <a style = {slideStyle} onClick={this.props.data.onClick}> this.props.data.text </a>
     }
 });
 
